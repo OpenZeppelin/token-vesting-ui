b7644dab586334d9b7631f3eb7e29fee238b047c
Martín Triay <martriay@gmail.com>
2017-10-18 20:12:26 -0300

    Add symlink resolution to require built contracts from src/

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-const TokenVesting = artifacts.require("TokenVesting");
-const SimpleToken = artifacts.require("SimpleToken");
+const TokenVesting = artifacts.require("TokenVesting")
+const SimpleToken = artifacts.require("SimpleToken")
 
 module.exports = function(deployer) {
   const beneficiary = "0x1E6876a6C2757de611c9F12B23211dBaBd1C9028"
@@ -8,6 +8,6 @@ module.exports = function(deployer) {
   const cliff = 30 // ~1 yr
   const duration = 60 * 60 // ~4yrs
 
-  deployer.deploy(TokenVesting, beneficiary, start, cliff, duration, false);
-  deployer.deploy(SimpleToken);
+  deployer.deploy(TokenVesting, beneficiary, start, cliff, duration, false)
+  deployer.deploy(SimpleToken)
 };
