b0768abecc7167af9252ab209301affbc39faa9d
Martín Triay <martriay@gmail.com>
2017-10-19 15:36:08 -0300

    Add no funds warning

--- src/VestingSchedule.jsx
+++ src/VestingSchedule.jsx
@@ -8,16 +8,27 @@ function VestingSchedule({ details }) {
     <div>
       <h4>Vesting schedule</h4>
       { ! details.revoked
-          ? <VestingChart details={ details } />
+          ? details.total > 0
+            ? <VestingChart details={ details } />
+            : <Empty />
           : <Revoked />
       }
     </div>
   )
 }
 
+function Empty() {
+  return <div className="warning">
+    <span className="warning-message">
+      <Emoji e="⚠️" /> No funds in the contract
+    </span>
+    <VestingChart details={ {} } />
+  </div>
+}
+
 function Revoked() {
-  return <div className="revoked">
-    <span className="revoked-message">
+  return <div className="warning">
+    <span className="warning-message">
       <Emoji e="⚠️" /> Revoked
     </span>
     <VestingChart details={ {} } />
